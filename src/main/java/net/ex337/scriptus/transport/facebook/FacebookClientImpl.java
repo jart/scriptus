6b32f43f1aa35fc3097ac836f3b0fa037983f65d
Andres Perez <andres.perez@mail.polimi.it>
2012-07-15 12:14:23 +0200

    Added logging to the methods and fixed some minor issues

--- src/main/java/net/ex337/scriptus/transport/facebook/FacebookClientImpl.java
+++ src/main/java/net/ex337/scriptus/transport/facebook/FacebookClientImpl.java
@@ -47,27 +47,26 @@ public class FacebookClientImpl implements FacebookClientInterface {
 
 	@Override
 	public List<FacebookPost> getRecentPosts(Long sinceTime) {
-		List<FacebookPost> mentions = new ArrayList<FacebookPost>();
+		List<FacebookPost> recentPosts = new ArrayList<FacebookPost>();
 
+		LOG.info("Start getRecentPosts");
 		/**
 		 * Get mentions
 		 */
 		Connection<Post> connections = null;
-		List<Post> tags = new ArrayList<Post>();
 		List<Post> posts = new ArrayList<Post>();
 		if (sinceTime == null) {
 			connections = facebookClient
 					.fetchConnection("me/posts", Post.class);
-			posts = connections.getData();
-			tags.addAll(posts);
+			posts.addAll(connections.getData());
 		} else {
 			connections = facebookClient.fetchConnection("me/posts",
 					Post.class, Parameter.with("limit", "25"),
 					Parameter.with("since", sinceTime));
-			posts = connections.getData();
-			tags.addAll(posts);
+			posts.addAll(connections.getData());
 		}
-		for (Post p : tags) {
+		LOG.info("Retrieved [" + posts.size() + "] posts");
+		for (Post p : posts) {
 			// Ghhr !! facebook graph api is not working well !! until parameter
 			// is not being taken in count, i am handling it
 			if (p.getMessage() != null
@@ -81,24 +80,36 @@ public class FacebookClientImpl implements FacebookClientInterface {
 						.getId() : sender.getUsername();
 				FacebookPost fbp = new FacebookPost(p.getId(), p.getMessage(),
 						screenName, p.getCreatedTime().getTime());
-				mentions.add(fbp);
+				recentPosts.add(fbp);
 			}
 		}
-		// When untiltime is null limit results to the most recent one
+		LOG.info("After filtering by since time got [" + recentPosts.size()
+				+ "] posts");
+		// When sinceTime is null limit results to the most recent one
 		if (sinceTime == null) {
+			LOG.info("Retaining only most recent posts");
 			// Retain last post and discard all others
-			FacebookPost[] lastPostToRetain = { Collections.max(mentions) };
-			mentions.retainAll(Arrays.asList(lastPostToRetain));
+			FacebookPost[] lastPostToRetain = { Collections.max(recentPosts) };
+			recentPosts.retainAll(Arrays.asList(lastPostToRetain));
 		}
-		return mentions;
+		LOG.info("End getRecentPosts");
+		return recentPosts;
 	}
 
 	@Override
 	public List<FacebookPost> getPostComments(String postId, Long sinceTime) {
+		LOG.info("Start getPostComments");
 		List<FacebookPost> messageReplies = new ArrayList<FacebookPost>();
-		Connection<Comment> comments = facebookClient.fetchConnection(postId
-				+ "/comments", Comment.class, Parameter.with("limit", "25"),
-				Parameter.with("since", sinceTime));
+		Connection<Comment> comments;
+		if (sinceTime == null) {
+			comments = facebookClient.fetchConnection(postId + "/comments",
+					Comment.class);
+		} else {
+			comments = facebookClient.fetchConnection(postId + "/comments",
+					Comment.class, Parameter.with("limit", "25"),
+					Parameter.with("since", sinceTime));
+		}
+		LOG.info("Retrieved [" + comments.getData().size() + "] comments");
 		for (Comment c : comments.getData()) {
 			User sender = facebookClient.fetchObject(c.getFrom().getId(),
 					User.class);
@@ -108,6 +119,7 @@ public class FacebookClientImpl implements FacebookClientInterface {
 					screenName, c.getCreatedTime().getTime(), postId);
 			messageReplies.add(fbp);
 		}
+		LOG.info("End getPostComments");
 		return messageReplies;
 	}
 
@@ -144,11 +156,14 @@ public class FacebookClientImpl implements FacebookClientInterface {
 	}
 
 	public List<FacebookPost> getPostReplies() {
+		LOG.info("Start getPostReplies");
 		List<FacebookPost> postReplies = new ArrayList<FacebookPost>();
 
 		// Fetch all the new notifications
 		List<Notification> notifications = facebookClient.fetchConnection(
 				"me/notifications", Notification.class).getData();
+
+		LOG.info("Retrieved [" + notifications.size() + "] notifications");
 
 		// Iterate over them looking for notifications of replies to a comment
 		for (Notification notif : notifications) {
@@ -181,7 +196,6 @@ public class FacebookClientImpl implements FacebookClientInterface {
 						notif.getMessage(), replierScreenName, notif
 								.getCreatedTime().getTime(), p.getId());
 				postReplies.add(fbp);
-				// TODO Log this response
 				Boolean publishMessageResponse = facebookClient.publish(
 						notif.getId(), Boolean.class,
 						Parameter.with("unread", "0"));
@@ -191,42 +205,78 @@ public class FacebookClientImpl implements FacebookClientInterface {
 				}
 			}
 		}
+		LOG.info("After filtering by notification type and owenership got ["
+				+ postReplies.size() + "] comments");
+		LOG.info("End getPostReplies");
 		return postReplies;
 	}
 
 	@Override
 	public Long getTime(String mentionId) {
+		LOG.info("Start getTime");
 		Post p = facebookClient.fetchObject(mentionId, Post.class);
+		Long time = null;
 		if (p == null) {
 			Comment c = facebookClient.fetchObject(mentionId, Comment.class);
 			if (c == null) {
-				return null;
+				LOG.info("Facebook object [" + mentionId
+						+ "] is not a valid post/comment");
 			} else {
-				return c.getCreatedTime().getTime() / 1000L;
+				LOG.info("Facebook object [" + mentionId + "] is comment");
+				time = c.getCreatedTime() == null ? null : c.getCreatedTime()
+						.getTime() / 1000L;
 			}
 		} else {
-			return p.getCreatedTime().getTime() / 1000L;
+			LOG.info("Facebook object [" + mentionId + "] is post");
+			time = p.getCreatedTime() == null ? null : p.getCreatedTime()
+					.getTime() / 1000L;
 		}
+		LOG.info("End getTime");
+		return time;
 	}
 
 	@Override
 	public String publish(String to, String message) {
+		LOG.info("Start publish");
 		FacebookType publishMessageResponse = null;
 		if (to != null) {
 			User user = facebookClient.fetchObject(to, User.class);
 			publishMessageResponse = facebookClient.publish(user.getId()
 					+ "/feed", FacebookType.class,
 					Parameter.with("message", message));
-			return publishMessageResponse.getId();
 		} else {
 			publishMessageResponse = facebookClient.publish("me/feed",
 					FacebookType.class, Parameter.with("message", message));
 		}
-		// TODO log the success or failure of the publishing action
+		LOG.error("Published post [" + publishMessageResponse.getId() + "]");
+		LOG.info("End publish");
 		return publishMessageResponse.getId();
 	}
 
 	public static void main(String[] args) throws MalformedURLException {
-		FacebookClientImpl facebook = new FacebookClientImpl();
+		// FacebookClientImpl facebook = new FacebookClientImpl();
+		// facebook.RecentPosts(new Long(1341536400));
+		// facebook.getPostComments("565534114_10151233794789115", new
+		// Long(1341586260));
+		// facebook.getPostReplies();
+		// facebook.getTime("123");
+		// List<String> list = new ArrayList<String>();
+		// list.add("a");
+		// list.add("b");
+		// try {
+		// String encodedStringList = Base64.encode(SerializableUtils
+		// .serialiseObject(list));
+		// System.out.println(encodedStringList);
+		// @SuppressWarnings("unchecked")
+		// List<String> decodedList = (List<String>) SerializableUtils
+		// .deserialiseObject(Base64.decode(encodedStringList));
+		// System.out.println(decodedList);
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// } catch (ClassNotFoundException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 	}
 }
