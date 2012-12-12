e746edf7cb8f367759f0e2e303effdebbe5cbf85
Ian Sollars <ian.sollars@gmail.com>
2012-12-12 12:37:23 +0100

    fix sample

--- src/main/resources/samples/addTwoNumbers.js
+++ src/main/resources/samples/addTwoNumbers.js
@@ -2,39 +2,20 @@
 var pid = scriptus.fork();
 
 if(pid == 0) {
-	return ask("foo", "give me your number");
+	return ask("give me your number", {to:"foo"});
 }
 
-sleep("1Y");
-
-var second = ask("bar", "give me second number");
+var second = ask("give me a second number", {to:"bar"});
 
 var first;
 
 var waitedfor = wait(function(result) {
-	say("bar", "in result fn, response from foo: "+result);
+	say("in result fn, response from foo: "+result, {to:"bar"});
 	first = result;
 });
-/*
-var f = eval(first);
 
-f();
+say("parent waited for pid "+waitedfor, {to:"foo"});
 
-pipe(function(in, out) {
+say("response from bar="+second+", from="+second.from+", cid="+second.cid, {to:"foo"});
 
-	var pid = fork();
-	
-	if(pid == 0) {
-		say(in, "sending in");
-		return;
-	}
-	
-	say(listen(out));
-
-})
-*/
-say("foo", "parent waited for pid "+waitedfor);
-
-say("foo", "response from bar="+second+", from="+second.from+", cid="+second.cid);
-
-say("foo", "foo and bar="+(new Number(first) + Number(second)));
+say("foo and bar="+(new Number(first) + Number(second)), {to:"foo"});
